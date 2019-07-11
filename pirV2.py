3d97ee6b96b0d54073cfe418d270accde771bcbb
Yasha261998 <49835962+Yasha261998@users.noreply.github.com>
2019-07-11 23:44:33 +0300

    Update pirV2.py

--- pirV2.py
+++ pirV2.py
@@ -19,33 +19,6 @@ LED_INVERT = False      # True для иняертирования сигнал�
 LED_CHANNEL = 0         # Set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
-def main():
-    GPIO.setup(PIR_PIN, GPIO.IN)
-    turned_off = False
-
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
-    strip.begin()
-    colors = Color(0, 0, 0)
-
-    print('Run')
-
-    last_motion_time = time.time()
-
-    while True:
-        if GPIO.input(PIR_PIN):
-            last_motion_time = time.time()
-            sys.stdout.flush()
-            if turned_off:
-                turned_off = False
-                colors = turn_on()
-        else:
-            if not turned_off and time.time() > (last_motion_time + SHUTOFF_DELAY):
-                turned_off = True
-                colors = turn_off()
-        time.sleep(.1)
-        colorWipe(strip, colors, wait_ms=100)  # Display color
-
-
 def turn_on():                # chto delat kogda dvizhenie obnaruzheno
     print "Motion DETECTED"
     return Color(77, 77, 255)
@@ -64,8 +37,31 @@ def colorWipe(strip, color, wait_ms=50):  # отображение цвета п
 
 
 if __name__ == '__main__':
+    GPIO.setup(PIR_PIN, GPIO.IN)
+    turned_off = False
+
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
+    strip.begin()
+    colors = Color(0, 0, 0)
+
+    print('Run')
+
+    last_motion_time = time.time()
+
     try:
-        main()
+        while True:
+            if GPIO.input(PIR_PIN):
+                last_motion_time = time.time()
+                sys.stdout.flush()
+                if turned_off:
+                    turned_off = False
+                    colors = turn_on()
+            else:
+                if not turned_off and time.time() > (last_motion_time + SHUTOFF_DELAY):
+                    turned_off = True
+                    colors = turn_off()
+            time.sleep(.1)
+            colorWipe(strip, colors, wait_ms=100)  # Display color
     except KeyboardInterrupt:
         GPIO.cleanup()
     colorWipe(strip, Color(0, 0, 0), 10)
